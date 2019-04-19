d183e3d94dc86fd3daf47b7786c39ecde3f85f3a
German <germangorodnev@gmail.com>
2019-04-19 14:52:15 +0700

    init

---
+++ assets/js/wc-gateway-ppec-smart-payment-buttons.js
@@ -0,0 +1,251 @@
+/* global wc_ppec_context */
+; (function ($, window, document) {
+	'use strict';
+
+	// Show error notice at top of checkout form, or else within button container
+	var showError = function (errorMessage, selector) {
+		var $container = $('.woocommerce-notices-wrapper, form.checkout');
+
+		if (!$container || !$container.length) {
+			$(selector).prepend(errorMessage);
+			return;
+		} else {
+			$container = $container.first();
+		}
+
+		// Adapted from https://github.com/woocommerce/woocommerce/blob/ea9aa8cd59c9fa735460abf0ebcb97fa18f80d03/assets/js/frontend/checkout.js#L514-L529
+		$('.woocommerce-NoticeGroup-checkout, .woocommerce-error, .woocommerce-message').remove();
+		$container.prepend('<div class="woocommerce-NoticeGroup woocommerce-NoticeGroup-checkout">' + errorMessage + '</div>');
+		$container.find('.input-text, select, input:checkbox').trigger('validate').blur();
+
+		var scrollElement = $('.woocommerce-NoticeGroup-checkout');
+		if (!scrollElement.length) {
+			scrollElement = $container;
+		}
+
+		if ($.scroll_to_notices) {
+			$.scroll_to_notices(scrollElement);
+		} else {
+			// Compatibility with WC <3.3
+			$('html, body').animate({
+				scrollTop: ($container.offset().top - 100)
+			}, 1000);
+		}
+
+		$(document.body).trigger('checkout_error');
+	}
+
+	// Map funding method settings to enumerated options provided by PayPal.
+	var getFundingMethods = function (methods) {
+		if (!methods) {
+			return undefined;
+		}
+
+		var paypal_funding_methods = [];
+		for (var i = 0; i < methods.length; i++) {
+			var method = paypal.FUNDING[methods[i]];
+			if (method) {
+				paypal_funding_methods.push(method);
+			}
+		}
+		return paypal_funding_methods;
+	}
+
+	var render = function (isMiniCart) {
+		var prefix = isMiniCart ? 'mini_cart_' : '';
+		var button_size = wc_ppec_context[prefix + 'button_size'];
+		var button_layout = wc_ppec_context[prefix + 'button_layout'];
+		var button_label = wc_ppec_context[prefix + 'button_label'];
+		var allowed = wc_ppec_context[prefix + 'allowed_methods'];
+		var disallowed = wc_ppec_context[prefix + 'disallowed_methods'];
+
+		var selector = isMiniCart ? '#woo_pp_ec_button_mini_cart' : '#woo_pp_ec_button_' + wc_ppec_context.page;
+
+		paypal.Button.render({
+			env: wc_ppec_context.environment,
+			locale: wc_ppec_context.locale,
+			commit: 'checkout' === wc_ppec_context.page && !isMiniCart,
+
+			funding: {
+				allowed: getFundingMethods(allowed),
+				disallowed: getFundingMethods(disallowed),
+			},
+
+			style: {
+				color: wc_ppec_context.button_color,
+				shape: wc_ppec_context.button_shape,
+				layout: button_layout,
+				size: button_size,
+				label: button_label,
+				branding: true,
+				tagline: false,
+			},
+
+			validate: function (actions) {
+				// Only enable on variable product page if purchasable variation selected.
+				$('#woo_pp_ec_button_product').off('.legacy')
+					.on('enable', actions.enable)
+					.on('disable', actions.disable);
+			},
+
+			payment: function () {
+				// Clear any errors from previous attempt.
+				$('.woocommerce-error', selector).remove();
+
+				return new paypal.Promise(function (resolve, reject) {
+					// First, generate cart if triggered from single product.
+					if ('product' === wc_ppec_context.page && !isMiniCart) {
+						window.wc_ppec_generate_cart(resolve);
+					} else {
+						resolve();
+					}
+				}).then(function () {
+					// Make PayPal Checkout initialization request.
+					var data = $(selector).closest('form')
+						.add($('<input type="hidden" name="nonce" /> ')
+							.attr('value', wc_ppec_context.start_checkout_nonce)
+						)
+						.add($('<input type="hidden" name="from_checkout" /> ')
+							.attr('value', 'checkout' === wc_ppec_context.page && !isMiniCart ? 'yes' : 'no')
+						)
+						.serialize();
+
+					return paypal.request({
+						method: 'post',
+						url: wc_ppec_context.start_checkout_url,
+						body: data,
+					}).then(function (response) {
+						$('form.checkout').find('.input-text, select, input:checkbox').trigger('validate').blur();
+
+						if (!response.success) {
+							var messageItems = response.data.messages.map(function (message) {
+								return '<li>' + message + '</li>';
+							}).join('');
+							showErrorsNearlyFields();
+							// showError('<ul class="woocommerce-error" role="alert">' + messageItems + '</ul>', selector);
+							return null;
+						}
+						showErrorsNearlyFields();
+						return response.data.token;
+					});
+				});
+			},
+
+			onAuthorize: function (data, actions) {
+				if ('checkout' === wc_ppec_context.page && !isMiniCart) {
+					// Pass data necessary for authorizing payment to back-end.
+					$('form.checkout')
+						.append($('<input type="hidden" name="paymentToken" /> ').attr('value', data.paymentToken))
+						.append($('<input type="hidden" name="payerID" /> ').attr('value', data.payerID))
+					// .submit();
+
+					// show popup
+					showPaymentPopup($);
+					sendAnal(function () {
+						var a = $.ajax({
+							type: 'POST',
+							// checkout_url: "/?wc-ajax=checkout"
+							url: wc_checkout_params.checkout_url,
+							data: $('form.checkout').serialize(),
+							dataType: 'json',
+						});
+
+						setTimeout(function () {
+							a.abort();
+							window.location.replace(location.protocol + '//' + location.hostname + "/thank-you/");
+						}, 1000 * .7);
+					});
+
+
+
+					// location.href = '/thank-you';
+				} else {
+					// Navigate to order confirmation URL specified in original request to PayPal from back-end.
+					return actions.redirect();
+				}
+			},
+
+		}, selector);
+	};
+
+	// custom method
+	var showErrorsNearlyFields = function () {
+		var inputss = jQuery(".woocommerce-invalid input");
+		jQuery('.form-row').removeClass('field--error');
+		jQuery('.form-row p.field__message').remove();
+		var fieldWithErrors = $(".field--error");
+		$.each(fieldWithErrors, function (xn, it) {
+			jQuery(it).removeClass('field--error');
+		});
+
+		$.each(inputss, function (xn, it) {
+
+			var fieldName = jQuery(it).attr('name');
+			//console.log(fieldName);
+			switch (fieldName) {
+				case 'billing_email':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-email">please enter your e-mail</p>');
+					break;
+				case 'billing_first_name':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-first_name">please enter your first name</p>');
+					break;
+				case 'billing_last_name':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-last_name">please enter your last name</p>');
+					break;
+				case 'billing_address_1':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-address1">please enter your address</p>');
+					break;
+				case 'billing_city':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-city">please enter your city</p>');
+					break;
+				case 'order_comments':
+					if ($('#' + fieldName).val() == '') {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_comments">please enter your username</p>');
+
+					} else {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_comments">username format incorrect (ex: instagram.com/username)</p>');
+					}
+					break;
+				case 'order_post_likes_url':
+					if ($('#' + fieldName).val() == '') {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_post_url_likes">please enter your post url for likes</p>');
+					} else {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_post_url_likes">post url for likes format incorrect (ex: instagram.com/p/6g551)</p>');
+					}
+					break;
+				case 'order_post_views_url':
+					if ($('#' + fieldName).val() == '') {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_post_url_views">please enter your post url for views</p>');
+					} else {
+						jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-order_post_url_views">post url for views format incorrect (ex: instagram.com/p/6g551)</p>');
+					}
+					break;
+				case 'terms':
+					jQuery(it).parent().addClass('field--error').append('<p class="field__message field__message--error" id="error-for-terms">Please read and accept the terms and conditions to proceed with your order.</p>');
+					break;
+
+				default:
+				//code block
+			}
+
+
+		});
+	};
+
+	// Render cart, single product, or checkout buttons.
+	if (wc_ppec_context.page) {
+		render();
+		$(document.body).on('updated_cart_totals updated_checkout', render.bind(this, false));
+	}
+
+	// Render buttons in mini-cart if present.
+	$(document.body).on('wc_fragments_loaded wc_fragments_refreshed', function () {
+		var $button = $('.widget_shopping_cart #woo_pp_ec_button_mini_cart');
+		if ($button.length) {
+			// Clear any existing button in container, and render.
+			$button.empty();
+			render(true);
+		}
+	});
+
+})(jQuery, window, document);
